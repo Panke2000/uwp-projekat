787c4d5cc64a4a1b4b343b81bdb2f0270910cec1
Panke2000 <jovan.panic@pmf.edu.rs>
2023-06-03 08:18:28 +0200

    nazad na vrh dugme popravljeno i jos neke stvari

--- Script&Style/main.js
+++ Script&Style/main.js
@@ -1,8 +1,9 @@
 /* HEADER */
 
-let headerLinks = this.document.querySelectorAll('#header-links a');
+let headerLinks = document.querySelectorAll('#header-links a');
 let hamburgerMenu = document.querySelector('#hamburger-menu');
 let lines = document.querySelectorAll('.line');
+let dugmeNaVrh = document.getElementById("nazad-na-vrh"); // nazad na vrh dugme
 
 console.log(headerLinks);
 
@@ -12,8 +13,9 @@ hamburgerMenu.addEventListener('click', function() {
 });
 
 window.addEventListener('scroll', function() {
-    let header = this.document.querySelector('header');
-    if (this.window.scrollY > header.offsetTop) {
+    let header = document.querySelector('header'); // header animacija
+    
+    if (window.scrollY > header.offsetTop) { // header animacija
         header.style.backgroundColor = 'white';
         header.style.boxShadow = '0px 0px 10px 0px rgba(0,0,0,0.75)'
         headerLinks.forEach(element => {
@@ -21,7 +23,7 @@ window.addEventListener('scroll', function() {
             element.style.borderColor = 'black';
             element.style.color = 'black';
         });
-    } else {
+    } else { // header animacija
         header.style.backgroundColor = 'var(--color-A)';
         header.style.borderBottom = 'none';
         header.style.boxShadow = 'none'
@@ -31,6 +33,7 @@ window.addEventListener('scroll', function() {
             element.style.color = 'black';
         });
     }
+    pokaziDugme(); // nazad na vrh dugme
 });
 
 headerLinks.forEach(link => {
@@ -52,5 +55,28 @@ headerLinks.forEach(link => {
           }
     });
   });
+
+
+
+
+
+/*  NAZAD NA VRH DUGME  */
+
+
+// pokazuje-skriva dugme
+function pokaziDugme() {
+  if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+    dugmeNaVrh.style.display = "block";
+  } else {
+    dugmeNaVrh.style.display = "none";
+  }
+}
+
+// pritiskom na dugme vraca na vrh stranice
+function naVrh() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox...
+}
+
 
   
