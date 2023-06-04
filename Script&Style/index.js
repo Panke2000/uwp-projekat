0bad8befdff89ec9438306c0d039689de4e1087d
Panke2000 <jovan.panic@pmf.edu.rs>
2023-06-04 21:25:30 +0200

    finished homepage

--- Script&Style/index.js
+++ Script&Style/index.js
@@ -6,11 +6,17 @@ let nagrada2 = document.getElementById('nagrada-2');
 let nagrada3 = document.getElementById('nagrada-3');
 let last = 1;
 
-console.log(nagrada1);
+let slika = document.getElementById('slika');
+let slikaSRC = document.getElementById('slika').attributes[1];
+let ime = document.getElementById('ime');
+let lastImage = 1;
+
+console.log(slikaSRC);
 
 setInterval(function() {
   switch (last) {
     case 1:
+
       fadeOut(nagrada1, function() {
         if (nagrada1.innerHTML === '- "Najbolji kozmeticki salon na teritoriji juzne Srbije" ') {
           nagrada1.innerHTML = '- "Sydney Sweeney 2020"';
@@ -19,9 +25,12 @@ setInterval(function() {
         }
         fadeIn(nagrada1);
       });
+
       last = 2;
       break;
+
     case 2:
+
       fadeOut(nagrada2, function() {
         if (nagrada2.innerHTML === '- "Profesionalno osoblje" za tri uzastopne godine ') {
           nagrada2.innerHTML = '- "Milano summit 2021"';
@@ -30,9 +39,12 @@ setInterval(function() {
         }
         fadeIn(nagrada2);
       });
+
       last = 3;
       break;
+
     case 3:
+
       fadeOut(nagrada3, function() {
         if (nagrada3.innerHTML === '- "Manikir professional by Francais beaute"') {
           nagrada3.innerHTML = '- "Beauty salon Awards 2022"';
@@ -41,10 +53,43 @@ setInterval(function() {
         }
         fadeIn(nagrada3);
       });
+
       last = 1;
       break;
   }
 }, 2000);
+
+setInterval(function() {
+  switch (lastImage) {
+    case 1:
+      fadeOut(slika, function() {
+        slikaSRC.textContent = 'Materijal/galena.jpg';
+        ime.innerHTML = 'Galena';
+        fadeIn(slika);
+      });
+
+      lastImage = 2;
+    break;
+    case 2:
+      fadeOut(slika, function() {
+        slikaSRC.textContent = 'Materijal/jennifer-aniston.jpg';
+        ime.innerHTML = 'Jennifer Aniston';
+        fadeIn(slika);
+      });
+
+      lastImage = 3;
+    break;
+    case 3:
+      fadeOut(slika, function() {
+        slikaSRC.textContent = 'Materijal/sydney-sweeney.jpg';
+        ime.innerHTML = 'Sydney Sweeney';
+        fadeIn(slika);
+      });
+
+      lastImage = 1;
+    break;
+  }
+}, 4000);
 
 function fadeOut(element, callback) {
   let opacity = 1;
